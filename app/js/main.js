e4b0030c5ccb28eb81bebf365702c95d960876a2
Maurice Svay <maurice@svay.com>
2016-06-10 23:36:25 +0200

    Adds model sorting
    Fixes #2

--- app/js/main.js
+++ app/js/main.js
@@ -11,4 +11,26 @@ $(document).ready(function(){
             window.location.reload();
         });
     });
+
+    function getSortFunction(attribute) {
+        switch (attribute) {
+            case 'data-date':
+                return function(a,b) {
+                    return (new Date(b.elm.getAttribute(attribute))) - (new Date(a.elm.getAttribute(attribute)));
+                };
+            case 'data-views':
+            case 'data-likes':
+                return function(a,b){
+                    return parseInt(b.elm.getAttribute(attribute), 10) - parseInt(a.elm.getAttribute(attribute), 10);
+                };
+        }
+    }
+
+    $('button[data-action]').on('click', function(){
+        var $this = $(this);
+        var attribute = $this.attr('data-action').replace('sort', 'data');
+        $('button[data-action]').removeClass('active');
+        $this.addClass('active');
+        tinysort('ul.skfb-grid>li', { sortFunction: getSortFunction(attribute)});
+    });
 });
